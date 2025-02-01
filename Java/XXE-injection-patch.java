a68728c02bb5b14546e57165b1dcdbe1b1001421
Ryland Rubens <39800803+ryrubes@users.noreply.github.com>
2025-02-01 15:17:00 -0800

    Delete Java directory

--- Java/XXE-injection-patch.java
+++
@@ -1,9 +0,0 @@
-// Disable external entities when parsing XML to prevent XXE attacks.
-
-
-
-DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);  // Disable DOCTYPE
-factory.setFeature("http://xml.org/sax/features/external-general-entities", false);  // Disable external entities
-factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);  // Disable external parameter entities
-DocumentBuilder builder = factory.newDocumentBuilder();
